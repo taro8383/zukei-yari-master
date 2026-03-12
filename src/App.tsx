ca05cc95e863dd5db98e4c5dde19f286ddcc63c5
Julian Oscar <taro.sirius@gmail.com>
2026-03-12 06:51:42 +0100

    Add basename to HashRouter for subdirectory hosting

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <HashRouter basename="/zukei-yari-master">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
