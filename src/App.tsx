592fd757dc9bd98c9fdc390c769c2c54f53a9bbb
Julian Oscar <taro.sirius@gmail.com>
2026-03-12 07:04:41 +0100

    Add 404 redirect and remove HashRouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename="/zukei-yari-master">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
